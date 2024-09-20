9debf3b6a5402728afc915d15d5c0b630929a81a
Justin Kim <115704061+goldenRabbit-23@users.noreply.github.com>
2024-09-20 19:45:11 +0900

    [Silver II] Title: A → B, Time: 0 ms, Memory: 2284 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.cc
+++ 백준/Silver/16953. A → B/A → B.cc
@@ -13,8 +13,6 @@
 #include <vector>
 using namespace std;
 
-const int INF = int(1e9) + 5;
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -22,36 +20,26 @@ int main() {
     int64_t A, B;
     cin >> A >> B;
 
-    map<int64_t, int> ops;
-    ops[A] = 1;
-
-    queue<int64_t> q;
-    q.push(A);
+    queue<pair<int64_t, int>> q;
+    q.push({A, 1});
 
     while (!q.empty()) {
-        int64_t now = q.front(); q.pop();
+        auto [now, cnt] = q.front(); q.pop();
 
-        if (now == B)
-            continue;
+        if (now == B) {
+            cout << cnt << '\n';
+            return 0;
+        }
 
         int64_t opt1 = now * 2, opt2 = now * 10 + 1;
 
-        if (opt1 <= B && ops.find(opt1) == ops.end())
-            ops[opt1] = INF;
-        if (opt2 <= B && ops.find(opt2) == ops.end())
-            ops[opt2] = INF;
-
-        if (opt1 <= B && ops[now] + 1 < ops[opt1]) {
-            ops[opt1] = ops[now] + 1;
-            q.push(opt1);
-        }
-        if (opt2 <= B && ops[now] + 1 < ops[opt2]) {
-            ops[opt2] = ops[now] + 1;
-            q.push(opt2);
-        }
+        if (opt1 <= B)
+            q.push({opt1, cnt + 1});
+        if (opt2 <= B)
+            q.push({opt2, cnt + 1});
     }
 
-    cout << (ops.find(B) == ops.end() ? -1 : ops[B]) << '\n';
+    cout << -1 << '\n';
 
     return 0;
 }
